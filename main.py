ddda6441be812457b2c2d4a5b2b5ac257b3df5a2
Artyom Sirekanyan <art.sir.sirekanyan2001@gmail.com>
2024-02-17 12:24:46 +0400

    Add env_manipulator module

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from vectordb import MiniDB
 from loader import Loader
+import env_manipulator
+
 
 if __name__ == "__main__":
-    mini_db = MiniDB()
-    mini_db.create(Loader("manuals/git.pdf"))
-   # print(mini_db.load())
+    env_manipulator.configure_environment()
+    #mini_db = MiniDB()
+    #mini_db.create(Loader("manuals/git.pdf"))
+
+
 
 
--- vectordb.py
+++ vectordb.py
@@ -32,3 +32,6 @@ class MiniDB:
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
         return cls.__instance
+
+    def get_vector_store(self):
+        return self.__db
