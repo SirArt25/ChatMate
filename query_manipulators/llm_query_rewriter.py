8e9b7536c3ab326451eea811af6868ea9dd4748c
Artyom Sirekanyan <art.sir.sirekanyan2001@gmail.com>
2024-02-25 16:19:47 +0400

    Minor Changes

--- query_manipulators/llm_query_rewriter.py
+++ query_manipulators/llm_query_rewriter.py
@@ -3,6 +3,8 @@ from langchain_core.prompts import PromptTemplate
 
 from query_manipulators.query_rewriter import QueryRewriter
 from pyrsistent import freeze
+
+
 class LLMQueryRewriter(QueryRewriter):
     def __init__(self, llm: BaseLanguageModel):
         self.__initializer(freeze(llm))
@@ -11,7 +13,8 @@ class LLMQueryRewriter(QueryRewriter):
         self.__template = """Use the following pieces of question to rewrite and correct the question at the end.
                         {question}"""
         self.__llm = llm
+
     def rewrite(self, query: str) -> str:
         query_prompt = PromptTemplate.from_template(self.__template)
-        formated_query = query_prompt.format(question=query)
-        return self.__llm.invoke(formated_query)
+        formatted_query = query_prompt.format(question=query)
+        return self.__llm.invoke(formatted_query)
